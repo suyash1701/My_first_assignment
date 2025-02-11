a670460e81d5538b3623119b2d367a54cfcc3ca0
suyash1701 <hardassuyash@gmail.com>
2025-02-11 17:15:51 +0530

    user input update

---
+++ user_input.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter your name:"
+read name
+echo "my name is $name"
+
+echo "Enter your age:"
+read age
+echo "my age is $age"
+
+echo "Enter your address:"
+read address
+echo "I reside in $adddress"
+
+echo "Enter your hooby:"
+read hobby
+echo "I like to do $hobby"
